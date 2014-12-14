039a77077b695b8fe5a9aa409cc2d1d841533b06
Raine Edrosa <ltedrosa@gmail.com>
2014-12-14 13:20:16 +0800

    second test: checking for home page contents

--- lists/views.py
+++ lists/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<html><title>To-Do List</title></html>");
+    #return HttpResponse("<html><title>To-Do List</title></html>");
+    context = {'test': 'test'}
+    return render(request, 'lists/index.html', context)
 
--- test_functional.py
+++ test_functional.py
@@ -16,6 +16,11 @@ class FunctionalTest(unittest.TestCase):
 
         # page should load the To-Do List landing page
         self.assertIn('To-Do List', self.browser.title)
+
+        self.assertIn('Welcome to To-Do List!', self.browser.find_element_by_tag_name("body").text);
+        self.assertIn('Create an Account', self.browser.find_element_by_tag_name("body").text);
+        self.assertIn('Log In', self.browser.find_element_by_tag_name("body").text);
+
         self.fail('Finish the test!')
 
 if __name__ == '__main__':
