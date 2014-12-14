34787e86ca406fff0efe1ab85280e0d45f635f03
Raine Edrosa <ltedrosa@gmail.com>
2014-12-15 00:41:24 +0800

    test: can create a list item

--- test_functional.py
+++ test_functional.py
@@ -9,10 +9,11 @@ class FunctionalTest(unittest.TestCase):
         self.browser.implicitly_wait(3)
 
     def tearDown(self):
+        self.browser.get('http://localhost:8000/delete_test')
         self.browser.quit()
         User.objects.all().delete()
 
-    def test_can_signup(self):
+    def Test_signup(self):
         # check homepage
         self.browser.get('http://localhost:8000')
 
@@ -64,7 +65,7 @@ class FunctionalTest(unittest.TestCase):
 
         #self.fail('Finish the test!')
 
-    def test_can_login(self):
+    def Test_login(self):
         self.browser.get('http://localhost:8000')
         # fill up and submit the form (valid input first)
         self.browser.find_element_by_name("name").send_keys("Raine")
@@ -121,7 +122,54 @@ class FunctionalTest(unittest.TestCase):
         # manually delete created user in production
         self.browser.get('http://localhost:8000/delete_test')
         
+
+    def Test_add_list_item(self):
+        self.browser.get('http://localhost:8000')
+        # fill up and submit the form (valid input first)
+        self.browser.find_element_by_name("name").send_keys("Raine")
+        self.browser.find_element_by_name("username").send_keys("raineaway")
+        self.browser.find_element_by_name("email").send_keys("ltedrosa@gmail.com")
+        self.browser.find_element_by_name("password").send_keys("password")
+        self.browser.find_element_by_name("confirm_password").send_keys("password")
+        self.browser.find_element_by_class_name("submit").click()
+
+        # after submitting, should be redirected to successfully signed up page
+        self.browser.implicitly_wait(3)
+
+        # click on Start my to-do list; should be redirected to home page
+        self.browser.find_element_by_tag_name("a").click()
+        self.browser.implicitly_wait(3)
+
+        self.assertIn('Today', self.browser.find_element_by_tag_name("body").text)
+        self.assertIn(datetime.date.today(), self.browser.find_element_by_tag_name("body").text)
+
+        self.assertIn('You have no to-do items yet.', self.browser.find_element_by_tag_name("body").text)
+        self.assertIn('Create one now.', self.browser.find_element_by_tag_name("body").text)
+
+        self.browser.find_element_by_class_name("create").click()
+
+        self.browser.implicitly_wait(3)
+
+        self.browser.assertIn('http://localhost:8000/create_item', self.browser.current_url)
+        self.browser.assertIn('Create a Task', self.browser.find_element_by_tag_name("body").text)
+        self.assertTrue(self.browser.find_element_by_name("task_name").size > 1)
+        self.assertTrue(self.browser.find_element_by_xpath("//input[@value='Create']").size > 1)
+
+        self.browser.find_element_by_name("task_name").send_keys("Finish the project")
+        self.browser.find_element_by_xpath("//input[@value='Create']").click()
         
+        self.browser.implicitly_wait(3)
+
+        self.browser.assertIn('http://localhost:8000', self.browser.current_url)
+        self.assertIn('Finish the project', self.browser.find_element_by_tag_name("body").text)
+
+        self.browser.get('http://localhost:8000/delete_test')
+        
+
+    def test_order(self):
+        self.Test_signup()
+        self.Test_login()
+        self.Test_add_list_item()
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
