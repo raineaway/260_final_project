13b7b918a45ec0f83849f6c565f541ce32a9f0e9
Raine Edrosa <ltedrosa@gmail.com>
2014-12-14 13:35:16 +0800

    initial test for sign up page

--- test_functional.py
+++ test_functional.py
@@ -17,9 +17,17 @@ class FunctionalTest(unittest.TestCase):
         # page should load the To-Do List landing page
         self.assertIn('To-Do List', self.browser.title)
 
-        self.assertIn('Welcome to To-Do List!', self.browser.find_element_by_tag_name("body").text);
-        self.assertIn('Create an Account', self.browser.find_element_by_tag_name("body").text);
-        self.assertIn('Log In', self.browser.find_element_by_tag_name("body").text);
+        self.assertIn('Welcome to To-Do List!', self.browser.find_element_by_tag_name("body").text)
+        self.assertIn('Create an Account', self.browser.find_element_by_tag_name("body").text)
+        self.assertIn('Log In', self.browser.find_element_by_tag_name("body").text)
+
+        # clicking on Create an Account should redirect to sign up page
+        self.browser.find_element_by_link_text('Create an Account').click()
+        self.browser.implicitly_wait(3)
+        self.assertEqual('http://localhost:8000/signup', self.browser.current_url)
+
+        # sign up page should contain the sign up form
+        self.assertEqual('Sign Up | To-Do List', self.browser.title)
 
         self.fail('Finish the test!')
 
